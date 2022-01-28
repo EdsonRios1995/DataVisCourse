035c4b83f1784c9e3f0592868a1a7b1ad435a71b
Edson Ríos <edsonrios1995.er@gmail.com>
2022-01-28 12:08:19 -0600

    adding first version

---
+++ cereal_csv.py
@@ -0,0 +1,9 @@
+
+
+# Open and read csv
+
+    # Read the header row first (skip this part if there is no header)
+
+    # Read through each row of data after the header
+
+        # Convert row to float and compare to grams of fiber
